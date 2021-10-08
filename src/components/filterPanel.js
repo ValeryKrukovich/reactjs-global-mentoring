796a8166f91d7ef079dc3c253054bc3bbf873665
Valery_Krukovich <Valery_Krukovich@epam.com>
2021-10-08 14:52:03 +0300

    Adds filter panel

--- src/components/filterPanel.js
+++ src/components/filterPanel.js
@@ -4,6 +4,21 @@ import '../css/filterPanel.css';
 export default function FilterPanel() {
     return (
         <div className='filter-panel'>
+            <div className='button-list'>
+                <button type='button' className='button-list__button button-list__button_active'>All</button>
+                <button type='button' className='button-list__button'>Documentary</button>
+                <button type='button' className='button-list__button'>Comedy</button>
+                <button type='button' className='button-list__button'>Horror</button>
+                <button type='button' className='button-list__button'>Crime</button>
+            </div>
+            <div className='sort-select'>
+                <label className='sort-select__label'>Sort by</label>
+                <select className='sort-select__select'>
+                    <option value='value1'>release date</option>
+                    <option value='value2'>release date2</option>
+                    <option value='value3'>release date 3</option>
+                </select>
+            </div>
         </div>
     )
 }
